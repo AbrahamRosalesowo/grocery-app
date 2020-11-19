2624c0c60a14fa464d0bbc2af728b4583ff2417c
AbrahamRosalesuwu <abrahamreznov@outlook.com>
2020-11-18 22:40:52 -0600

    config vars created

--- global/global.js
+++ global/global.js
@@ -6,12 +6,13 @@
 
 */
 
-//process.env.NODE_ENV = process.env.NODE_ENV || "dev";
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
-let urlDB = "mongodb+srv://strider:paj7hZK1ZP5dFFLK@cluster0.4n4xs.mongodb.net/ts_crud?retryWrites=true&w=majority";
+let urlDB;
 if(process.env.NODE_ENV === "dev"){
     urlDB = "mongodb://localhost:27017/todo"
 }else{
-    urlDB = "mongodb+srv://strider:paj7hZK1ZP5dFFLK@cluster0.4n4xs.mongodb.net/ts_crud?retryWrites=true&w=majority"
+    urlDB = MONGO
+    //"mongodb+srv://strider:paj7hZK1ZP5dFFLK@cluster0.4n4xs.mongodb.net/ts_crud?retryWrites=true&w=majority"
 }
 process.env.URLDB = urlDB;
