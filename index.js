df454e91eec91bfa1d5b780f3aebed7309281324
AbrahamRosalesuwu <abrahamreznov@outlook.com>
2020-12-13 19:11:37 -0600

    cors access

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 app.use(require("./routes/index"));
 
 require("./global/global");
@@ -25,7 +34,7 @@ mongoose.connect(
   }
 );
 
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Server on port ${port}`);
